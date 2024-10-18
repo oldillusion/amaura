d49b205333c4af8496ebc55daaed5657963eb78b
oldillusion <gmccann@grademaker.com>
2024-10-18 16:34:17 +0100

    And a little more

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__, static_folder='public', static_url_path='/')
+
+@app.route("/")
+def index_html():
+    return redirect("/index.html")
